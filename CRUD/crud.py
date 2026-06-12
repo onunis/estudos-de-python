30e4c8e85d946b7ca8206955c1b0789365402d4b
Guilherme <mitaogui@gmail.com>
2026-06-12 00:38:47 -0300

    Feita as funcoes do CRUD

--- CRUD/crud.py
+++ CRUD/crud.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def init_db():
-    with sqlite3.connect("gastos.db") as conn:
+    with sqlite3.connect("crud.db") as conn:
         conn.execute("""
             CREATE TABLE IF NOT EXISTS categorias (
                 id_categoria INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,3 +19,35 @@ def init_db():
                 FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
             )
         """)
+
+def adicionar_categoria(nome):
+    with sqlite3.connect("crud.db") as conn:
+        conn.execute("INSERT INTO categorias (nome) VALUES (?)",(nome,))
+
+def listar_categorias():
+    with sqlite3.connect("crud.db") as conn:
+        return conn.execute(
+            "SELECT * FROM categorias"
+        ).fetchall()
+
+def adicionar_gasto(descricao, valor, data, id_categoria):
+    with sqlite3.connect("crud.db") as conn:
+        conn.execute("INSERT INTO gastos (descricao, valor, data, id_categoria) VALUES (?,?,?,?)",(descricao,valor,data, id_categoria))
+
+def listar_gastos():
+    with sqlite3.connect("crud.db") as conn:
+        return conn.execute(
+            "SELECT * FROM gastos"
+        ).fetchall()
+    
+def deletar_gasto(id_gasto):
+    with sqlite3.connect("crud.db") as conn:
+        conn.execute("DELETE FROM gastos WHERE (id_gasto) = (?)",(id_gasto,))
+
+def total_categoria():
+    with sqlite3.connect("crud.db") as conn:
+        return conn.execute("""SELECT categorias.nome, SUM(gastos.valor)
+                     FROM gastos
+                     INNER JOIN categorias ON gastos.id_categoria = categorias.id_categoria
+                     GROUP BY categorias.nome
+    """).fetchall()
