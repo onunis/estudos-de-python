d843bd65e3e4cc553b3dd0bc7267f3010424e31f
Guilherme <mitaogui@gmail.com>
2026-06-12 01:40:43 -0300

    Sisteminha CRUD no terminal completinho

--- CRUD/crud.py
+++ CRUD/crud.py
@@ -54,29 +54,59 @@ def total_categoria():
 
 
 def menu():
-    init_db():
+    init_db()
 
     while True:
-        print("\n1 - Adicionar Tarefas")
+        print("\n1 - Adicionar Categorias")
         print("\n2 - Listar Categorias")
         print("\n3 - Adicionar Gastos")
         print("\n4 - Listar Gastos")
         print("\n5 - Deletar Gastos")
         print("\n6 - Total por Categoria")
+        print("\n0 - Sair")
 
         opcao = input("\nEscolha: ")
 
         if opcao == "1":
-            nome = input("Nome da categoria: ")
+            nome = input("\nNome da categoria: ")
             adicionar_categoria(nome)
-            print("categoria adicionada!")
+            print("\ncategoria adicionada!")
 
         elif opcao == "2":
             categorias = listar_categorias()
             if not categorias:
-                print("Nenhuma categoria encontrada:")
+                print("\nNenhuma categoria encontrada.")
             else:
                 for id, nome in categorias:
                     print(f"{id} | {nome}")
 
-    
+        elif opcao == "3":
+            descricao = input("\nDescreva o gasto: ")
+            valor = int(input("\nQuanto foi gasto: "))
+            data = input("\nQuando foi feito esse gasto? ")
+            id_categoria = int(input("\nDigite o Id da categoria desejada: "))
+            adicionar_gasto(descricao, valor, data, id_categoria)
+
+        elif opcao == "4":
+            gastos = listar_gastos()
+            if not gastos:
+                print("\nNenhum gasto encontrado. Parabens!")
+            else:
+                for id, descricao, valor, data, id_categoria in gastos:
+                    print(f"{id} | {descricao} | R$:{valor} | {data} | cat:{id_categoria}")
+
+        elif opcao == "5":
+            delete_id = input("\nDigite o Id do Gasto que voce quer deletar: ")
+            deletar_gasto(int(delete_id))
+            print("Id Deletado. ")
+        
+        elif opcao == "6":
+            totais = total_categoria()
+            for nome, total in totais:
+                print(f"{nome} -> R${total:.2f}") 
+
+        elif opcao == "0":
+            break
+
+
+menu()
